a6b95311b4b0cde2d7cc86d1979f6045bdbc18e5
Yvo <yvo@uni-bremen.de>
2025-08-20 18:44:36 +0200

    build without compression

---
+++ Build/build.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bfe34f045cb51f53f2194e80ea529ebf0dd64d5417fa77738c05aabf2885dde7
+size 458833
--- Build/build.loader.js
+++ Build/build.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e10ea0b206c6563359478b109f3e13a782c3b6affaf18a1f596d2f41e803b193
-size 113760
+oid sha256:28fcc9c6119a623e7d2ff29c228b65abdf047dd381fcba8447e5172f0cc9c450
+size 20623
